cb7cddd7935c10cba33fc7069791f7070a802ebd
Senzo Khoza <sphiwe@thedigitalacademy.co.za>
2020-06-19 19:59:02 +0200

    ...

--- exercise-tracker/backend/routes/exercise.js
+++ exercise-tracker/backend/routes/exercise.js
@@ -8,5 +8,21 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/add').post((req,res) => {
-    const username = req.body.username
-})
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise ({
+        username,
+        description,
+        duration,
+        date,
+    });
+
+    newExercise.save()
+        .then(() => res.json('/Exercise added!'))
+        .catch(err => res.status(400).json('Error' + err))
+});
+
+module.exports = router;
